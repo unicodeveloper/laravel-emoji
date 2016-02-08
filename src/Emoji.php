bb57535d28d47abc7db2c93865206e29ec9a5a03
busayo <prosperotemuyiwa@gmail.com>
2016-02-08 07:26:44 +0100

    chore(emoji): :confetti_ball: :raised_hands: Add attributions

--- src/Emoji.php
+++ src/Emoji.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Laravel Emoji package.
+ *
+ * (c) Prosper Otemuyiwa <prosperotemuyiwa@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Unicodeveloper\Emoji;
 
 use Unicodeveloper\Emoji\Exceptions\{ UnknownMethod, UnknownEmoji, UnknownUnicode, IsNull };
